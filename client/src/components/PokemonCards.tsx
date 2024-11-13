42ac2257973097e48569da60f8f554db586f37d5
Martin Joey <joey.calypso@gmail.com>
2024-11-13 10:31:10 +0100

    Create Pokemon cards

---
+++ client/src/components/PokemonCards.tsx
@@ -0,0 +1,27 @@
+import type { Pokemon } from "../interface";
+import PokemonList from "./PokemonList";
+
+interface Props {
+  pokemons: Pokemon[];
+}
+
+function PokemonCards(Props: Props) {
+  const { pokemons } = Props;
+
+  return (
+    <section className="pokemonlist">
+      {pokemons.map((pokemon) => {
+        return (
+          <PokemonList
+            key={pokemon.id}
+            name={pokemon.name}
+            id={pokemon.id}
+            img={pokemon.sprites.front_default}
+          />
+        );
+      })}
+    </section>
+  );
+}
+
+export default PokemonCards;
