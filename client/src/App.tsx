f683e5d9aed7e574aa0f3f72f5291da7ed4e8540
Antoine Chauvot <antoine_chauvot@hotmail.com>
2024-11-15 12:40:44 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -100,14 +100,14 @@ function App() {
       <section className="pokeclosed-section">
         <Pokeclosed />
       </section>
-     <div className="app">
-       <nav className="search-bar-container">
-         <SearchBar
-           setResults={setResults}
-           onKeyNavigation={handleKeyNavigation}
-           setShowResults={setShowResults}
-         />
-         <SearchPokemonType />
+      <div className="app">
+        <nav className="search-bar-container">
+          <SearchBar
+            setResults={setResults}
+            onKeyNavigation={handleKeyNavigation}
+            setShowResults={setShowResults}
+          />
+          <SearchPokemonType />
           {/* Affiche la liste des résultats seulement si showResults est true */}
           {showResults && (
             <SearchResultsList
