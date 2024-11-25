75d10e9549a485a9e0c352ea11330a15c992117b
Martin Joey <joey.calypso@gmail.com>
2024-11-25 16:38:16 +0100

    add loader to trainer page

--- client/src/pages/TrainerList.tsx
+++ client/src/pages/TrainerList.tsx
@@ -1,22 +1,41 @@
+import { useEffect, useState } from "react";
 import trainersdata from "../../data/trainersdata.json";
 import TrainerCards from "../components/Trainercards";
 import "../components/Trainers.css";
+import Loader from "../components/Loader";
 
 function Trainerpage() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <main className="trainercards">
-      {trainersdata.map((trainer) => (
-        <TrainerCards
-          key={trainer.id}
-          trainername={trainer.trainername}
-          trainerimg={trainer.trainerimg}
-          trainerdescription={trainer.trainerdescription}
-          trainerfav1={trainer.trainerfav1}
-          trainerfav2={trainer.trainerfav2}
-          trainerfav3={trainer.trainerfav3}
-        />
-      ))}
-    </main>
+    <div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <main className="trainercards">
+            {trainersdata.map((trainer) => (
+              <TrainerCards
+                key={trainer.id}
+                trainername={trainer.trainername}
+                trainerimg={trainer.trainerimg}
+                trainerdescription={trainer.trainerdescription}
+                trainerfav1={trainer.trainerfav1}
+                trainerfav2={trainer.trainerfav2}
+                trainerfav3={trainer.trainerfav3}
+              />
+            ))}
+          </main>
+        </>
+      )}
+    </div>
   );
 }
 
